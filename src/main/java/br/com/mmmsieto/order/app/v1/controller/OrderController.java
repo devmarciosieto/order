1ea913b5d070c3ec7394fa668846ed6e769bbf59
Márcio Rogério Sieto <mmmsieto@hotmail.com>
2024-03-10 02:39:15 -0300

    add logs

--- src/main/java/br/com/mmmsieto/order/app/v1/controller/OrderController.java
+++ src/main/java/br/com/mmmsieto/order/app/v1/controller/OrderController.java
@@ -1,6 +1,8 @@
 package br.com.mmmsieto.order.app.v1.controller;
 
 import br.com.mmmsieto.order.domain.entity.OrderEntity;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,9 +17,13 @@ import java.util.UUID;
 @RestController
 public class OrderController {
 
+    private static final Logger log = LoggerFactory.getLogger(OrderController.class);
+
     @GetMapping
     @ResponseStatus(HttpStatus.OK)
     public OrderEntity index() {
+
+        log.info("create new order");
 
         return new OrderEntity(
                 UUID.randomUUID(),
